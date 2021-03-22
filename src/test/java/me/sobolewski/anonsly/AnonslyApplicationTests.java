a0d1d63e24f3a56cd45421e8940a2ae3c74fed05
Bartłomiej Sobolewski <bar.sobol00@gmail.com>
2021-03-22 17:18:28 +0100

    Initial commit

---
+++ src/test/java/me/sobolewski/anonsly/AnonslyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sobolewski.anonsly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonslyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
